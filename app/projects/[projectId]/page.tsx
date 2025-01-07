6de5c3a6a054ba3ab592d333d1674f5092994946
Waleed <waleed.nizamaniwn@gmail.com>
2025-01-07 21:39:28 +0100

    updated projects to better ui

--- app/projects/[projectId]/page.tsx
+++ app/projects/[projectId]/page.tsx
@@ -1,8 +1,14 @@
+import avatar from "../../../public/image.png";
 import ButtonBack from "@/components/ButtonBack";
 import { getSortedProjectsData, getProjectData } from "@/lib/projects";
 import { notFound } from "next/navigation";
+import Image from "next/image";
+import ButtonPersonal from "@/components/ButtonPersonal";
+import { CiLinkedin } from "react-icons/ci";
+import { IoMdHome } from "react-icons/io";
+import { IoLogoGithub, IoLogoGooglePlaystore } from "react-icons/io5";
 
-export default async function Project({ params }: {params : {projectId: string}}) {
+export default async function Project({ params }: { params: { projectId: string } }) {
     const projects = getSortedProjectsData();
     const projectId = params.projectId;
 
@@ -13,13 +19,78 @@ export default async function Project({ params }: {params : {projectId: string}}
     const project = await getProjectData(projectId);
 
     return (
-        <div className="max-w-screen-md container-md mx-auto px-8 flex flex-col justify-center dark:bg-dark-1 bg-grey-3 ">
-            <div className="p-8"></div>
-            {/* Back button */}
-            <ButtonBack />
-            <div className="markdown flex flex-col items-center">
-                <section dangerouslySetInnerHTML={{ __html: project }} />
+        <div className="max-w-screen-2xl mx-auto px-4 grid grid-cols-12 min-h-screen md:grid-cols-12">
+
+
+            <div className="col-span-12 md:col-span-7 lg:col-span-7  order-last md:order-none px-4 flex flex-col justify-center dark:bg-dark-1 bg-grey-3 ">
+                {/* Back button */}
+                <div className="p-5"></div>
+                <div className="md:block hidden">
+                    <ButtonPersonal
+                        text="Home"
+                        link="https://eyriscrafts.com"
+                        icon={<IoMdHome size={24} />}
+                        shouldOpenNewTab={false}
+                    />
+                </div>
+                <div className="p-5"></div>
+                <div className="markdown flex flex-col items-center">
+                    <section dangerouslySetInnerHTML={{ __html: project }} />
+                </div>
+
             </div>
+            <div className="lg:col-span-2 hidden lg:block">
+
+            </div>
+            <section className="col-span-12 md:col-span-5 lg:col-span-3 flex flex-col items-center px-4 xl:px-0 gap-y-4 order-first md:order-last">
+                <div className="p-8 md:p-10"></div>
+                <div className="md:hidden block w-full ">
+                    <ButtonPersonal
+                        text="Home"
+                        link="https://eyriscrafts.com"
+                        icon={<IoMdHome size={24} />}
+                        shouldOpenNewTab={false}
+                    />
+                </div>
+
+                {/* Personal Image */}
+                <Image
+                    style={{ width: "70%", maxWidth: "150px", aspectRatio: "1" }}
+                    src={avatar}
+                    alt="Picture of the author"
+                    className="rounded-full object-cover"
+                />
+
+                <h1 className="text-lg text-grey-2">
+                    {" "}
+                    Waleed K. Nizamani
+                </h1>
+
+                <p className="text-sm px-10 xl:px-6 text-grey-2 " >
+                    Software engineer with a keen interest in economy, science, and gaming
+                </p>
+
+                <p className="text-sm font-semibold text-gray-300 dark:text-gray-700" >
+                    More about me
+                </p>
+
+                {/* Playstore, github and Linkedin profiles */}
+                <div className="flex flex-wrap gap-2 justify-center">
+                    <ButtonPersonal
+                        text="Playstore"
+                        link="https://play.google.com/store/apps/developer?id=EyrisCraft"
+                        icon={<IoLogoGooglePlaystore size={24} />}
+                    />
+                    <ButtonPersonal
+                        link="https://github.com/EyrisCrafts"
+                        text="Github" icon={<IoLogoGithub size={24} />} />
+                    <ButtonPersonal
+                        link="https://www.linkedin.com/in/waleed-nizamani-957112104/"
+                        text="Linkedin"
+                        icon={<CiLinkedin size={24} />}
+                    />
+                </div>
+            </section>
 
         </div>
     );
